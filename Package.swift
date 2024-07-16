d23493342cd70dbd75a1d552ba0c20e1deb8b794
jiyong <wldyd2113@gmail.com>
2024-07-16 10:03:24 +0900

    데이터베이스 Fluent 라이브러리 추가 & 설정

--- Package.swift
+++ Package.swift
@@ -11,11 +11,18 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "4.99.3"),
         // 🔵 Non-blocking, event-driven networking for Swift. Used for custom executors
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
+        // 데이터베이스 모델 사용을 위한 라이브러리 추가
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
+        // 파일 데이터베이스(SQLite) 모듈 라이브러리 추가
+        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0")
     ],
     targets: [
         .executableTarget(
             name: "App",
             dependencies: [
+                // 라이브러리 임포트 설정
+                .product(name: "Fluent", package: "fluent"),
+                .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
                 .product(name: "Vapor", package: "vapor"),
                 .product(name: "NIOCore", package: "swift-nio"),
                 .product(name: "NIOPosix", package: "swift-nio"),
