067afa0c5e835a03914a2b79cae8e38bd2ce4671
jiyong <wldyd2113@gmail.com>
2024-07-16 09:35:54 +0900

    Model Entry파일 추가

---
+++ Sources/App/Models/Entry.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  
+//
+//  Created by 차지용 on 7/16/24.
+//
+
+import Vapor
+
+struct Entry: Content {
+    var id: UUID?
+    var title: String
+    var content: String
+    
+    init(id: UUID? = nil, title: String, content: String) {
+        self.id = id
+        self.title = title
+        self.content = content
+    }
+}
